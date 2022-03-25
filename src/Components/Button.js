b54ecc27279d38b374716622cff001ba5dc56455
游竣量 <jim@youjunliangdeMacBook-Pro.local>
2022-03-25 14:44:38 +0800

    first commit

---
+++ src/Components/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Button extends React.Component{
+    render(){
+        return(<button onClick={()=>this.props.setMode(this.props.mode)}>{this.props.name}</button>)
+    }
+}
+
+export default Button;
---
+++ src/Components/Item.js
@@ -0,0 +1,20 @@
+import React from "react";
+import X from "../x.png";
+
+class Item extends React.Component{
+    render(){
+        const doneStyle = {textDecorationLine: "line-through", opacity: "0.5"}
+        return(
+            <li className="todo-app__item">
+                <div className="todo-app__checkbox">
+                    <input type="checkbox" id={this.props.idx} defaultChecked={this.props.done} onClick={()=>this.props.checkBtn(this.props.idx, !this.props.done)}></input>
+                    <label htmlFor={this.props.idx}></label>
+                </div>
+                <h1 className="todo-app__item-detail" style={this.props.done?doneStyle:{}}>{this.props.item}</h1>
+                <img className="todo-app__item-x" src={X} alt="X" onClick={()=>this.props.deleteItem(this.props.idx)}/>
+            </li>
+        )
+    }
+}
+
+export default Item;
