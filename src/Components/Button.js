ec32846986d427d6d8801ca263cbf8cd71de50dd
游竣量 <jim@youjunliangdeMacBook-Pro.local>
2022-05-05 08:30:09 +0800

    1.0

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-class Button extends React.Component{
-    render(){
-        return(<button onClick={()=>this.props.setMode(this.props.mode)}>{this.props.name}</button>)
+class Button extends React.Component {
+    render() {
+        return (<button onClick={() => this.props.setMode(this.props.mode)}>{this.props.name}</button>);
     }
 }
 
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,76 +1,73 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Item from '../Components/Item';
 import Button from '../Components/Button';
 
-class App extends Component{
-    constructor(props){
+class App extends Component {
+    constructor(props) {
         super(props);
-        this.state = {todo_items:[], mode:0}
+        this.state = { todo_items: [], mode: 0 };
         this.index = 0;
     }
 
-    addItem = (e)=>{
-        let todo = e.target.value.trim()
-        if(e.code==="Enter"&&todo!==""){
+    addItem = (e) => {
+        let todo = e.target.value.trim();
+        if (e.code === "Enter" && todo !== "") {
             let idx = this.index;
-            this.setState((s)=>({todo_items:[...s.todo_items,{item:todo, done:false, idx:idx}]}));
+            this.setState((s) => ({ todo_items: [...s.todo_items, { item: todo, done: false, idx: idx }] }));
             this.index++;
-            e.target.value="";
+            e.target.value = "";
         }
     }
 
-    deleteItem = (idx)=>{
-        this.setState((s)=>({todo_items:s.todo_items.filter((i)=>i.idx!==idx)}))
-    }    
+    deleteItem = (idx) => {
+        this.setState((s) => ({ todo_items: s.todo_items.filter((i) => i.idx !== idx) }));
+    }
 
-    checkBtn = (idx, state)=>{
-        this.setState((s)=>{
-            s.todo_items.find((e)=>e.idx===idx).done = state;
-            return ({todo_items:[...s.todo_items]});
-            // return {};
+    checkBtn = (idx, state) => {
+        this.setState((s) => {
+            s.todo_items.find((e) => e.idx === idx).done = state;
+            return ({ todo_items: [...s.todo_items] });
         })
     }
 
-    displayIetm = (todo_items, mode)=>{
-        switch(mode){
+    displayIetm = (todo_items, mode) => {
+        switch (mode) {
             case 0:
-                return todo_items.map((i)=>(<Item key={i.idx} idx={i.idx} item={i.item} done={i.done} checkBtn={this.checkBtn} deleteItem={this.deleteItem}/>))
+                return todo_items.map((i) => (<Item key={i.idx} idx={i.idx} item={i.item} done={i.done} checkBtn={this.checkBtn} deleteItem={this.deleteItem}/>));
             case 1:
-                return todo_items.filter((i)=>i.done===false).map(i=>(<Item key={i.idx} idx={i.idx} item={i.item} done={i.done} checkBtn={this.checkBtn} deleteItem={this.deleteItem}/>))
-            case 2:
-                return todo_items.filter((i)=>i.done===true).map(i=>(<Item key={i.idx} idx={i.idx} item={i.item} done={i.done} checkBtn={this.checkBtn} deleteItem={this.deleteItem}/>))
-            default:
-                break;
+                return todo_items.filter((i) => i.done === false).map(i => (<Item key={i.idx} idx={i.idx} item={i.item} done={i.done} checkBtn={this.checkBtn} deleteItem={this.deleteItem}/>));
+            default:  // case 2
+                return todo_items.filter((i)=> i.done === true).map(i => (<Item key={i.idx} idx={i.idx} item={i.item} done={i.done} checkBtn={this.checkBtn} deleteItem={this.deleteItem}/>));
         }
     }
     
-    countItem = (todo_items)=>{
+    countItem = (todo_items) => {
         let left = 0;
         let completed = 0;
-        todo_items.forEach((i)=>{
-            if(i.done===false){
+        todo_items.forEach((i) => {
+            if (i.done === false) {
                 left++;
-            }else{
+            } else {
                 completed++;
             }
         });
-        return {left, completed};
+        return { left, completed };
     }
 
-    setMode = (mode)=>{
-        this.setState({mode:mode});
+    setMode = (mode) => {
+        this.setState({ mode: mode });
     }
 
-    clearCompleted = ()=>{
-        this.setState((s)=>({todo_items:s.todo_items.filter((i)=>i.done===false)}));
+    clearCompleted = () => {
+        this.setState((s) => ({ todo_items: s.todo_items.filter((i) => i.done === false) }));
     }
 
-    render(){
+    render() {
         const todo_items = this.state.todo_items;
         const mode = this.state.mode;
-        const {left, completed}= this.countItem(todo_items);
-        return(
-            <>            
+        const { left, completed } = this.countItem(todo_items);
+        return (
+            <>
             <header className="todo-app__header">
                 <h1 className="todo-app__title">todos</h1>
             </header>
@@ -79,11 +76,10 @@ class App extends Component{
                 <input className="todo-app__input" placeholder="What needs to be done?" onKeyDown={this.addItem}></input>
                 <ul className="todo-app__list" id="todo-list">
                     {this.displayIetm(todo_items, mode)}
-                    {/* {todo_items.map((i)=>(<Item key={i.idx} idx={i.idx} item={i.item} done={i.done} checkBtn={this.checkBtn}></Item>))} */}
                 </ul>
             </section>
 
-            {todo_items.length===0?<></>:
+            {todo_items.length === 0 ? <></> :
                 <footer className="todo-app__footer" id="todo-footer">
                     <div className="todo-app__total">{left} left</div>
                     <ul className="todo-app__view-buttons">
@@ -92,12 +88,12 @@ class App extends Component{
                         <Button name="Completed" mode={2} setMode={this.setMode}/>
                     </ul>
                     <div className="todo-app__clean">
-                        {completed>0?<button onClick={this.clearCompleted}>Clear Completed</button>:<button style={{visibility: "hidden"}}>Clear Completed</button>}
+                        {completed > 0 ? <button onClick={this.clearCompleted}>Clear Completed</button> : <button style={{ visibility: "hidden" }}>Clear Completed</button>}
                     </div>
                 </footer>
             }
             </>
-        )
+        );
     }
 }
 
